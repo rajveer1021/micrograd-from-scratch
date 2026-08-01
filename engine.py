3928f00b3e1768c446bd971970dcba0d860e828b
rajveer1021 <rajveermehta1021@gmail.com>
2026-08-02 00:40:05 +0530

    feat: store gradients in the Value nodes

--- engine.py
+++ engine.py
@@ -1,6 +1,8 @@
 class Value:
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
+        self.grad = 0.0
+        
         self._prev = set(_children)
         self._op = _op
         self.label = label
